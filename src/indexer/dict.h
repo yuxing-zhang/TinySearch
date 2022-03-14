27790abced1d85820e7ee0df21e50e6bffce950b
Yuxing Zhang <yzhang@cs.toronto.edu>
2022-03-14 02:13:04 -0400

    Add support of cleaning of nested dictionaries

---
+++ src/indexer/dict.h
@@ -0,0 +1,26 @@
+#define MAX_SLOT 10000
+
+// Linked list node
+typedef struct Node {
+    char *key;
+    void *value;
+    struct Node *next;
+} Node;
+
+Node *create_node(char *, void *, Node *);
+
+Node **create_dict();
+
+int has_key(Node *[], const char *);
+
+void *get_value(Node *[], const char *);
+
+void add_key(Node *[], char *, void *);
+
+void update_key(Node *[], const char *, void *);
+
+void free_node(Node *, int);
+
+void free_list(Node *, int);
+
+void free_dict(Node *[], int);
